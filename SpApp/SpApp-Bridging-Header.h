0867db20d21e0500a19c6167e1878328939e92fa
Tanaka Hayate <pinqphayate@gmail.com>
2016-10-20 20:35:04 +0900

    first commit

---
+++ SpApp/SpApp-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SpApp-Bridging-Header.h
+//  SpApp
+//
+//  Created by Tanaka Hayate on 2016/10/19.
+//  Copyright © 2016 Tanaka Hayate. All rights reserved.
+//
+
+#ifndef SpApp_Bridging_Header_h
+#define SpApp_Bridging_Header_h
+#import <RobotKit/RobotKit.h>
+
+#endif /* SpApp_Bridging_Header_h */
---
+++ SpAppTests/SpAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  SpAppTests.swift
+//  SpAppTests
+//
+//  Created by Tanaka Hayate on 2016/10/19.
+//  Copyright © 2016 Tanaka Hayate. All rights reserved.
+//
+
+import XCTest
+@testable import SpApp
+
+class SpAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
